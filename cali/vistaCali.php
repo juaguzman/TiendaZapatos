1fdb199034bc6bb710a11e3dd27c55c3882654de
juaguzman <juancarlos_891111@hotmail.com>
2015-11-24 17:17:05 -0500

    elminar zapatos

--- cali/vistaCali.php
+++ cali/vistaCali.php
@@ -15,7 +15,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php include ("../cali/caliIndex.php"); ?>
+        <?php include ("./headerC.php"); ?>
         
         <div>
             
--- modelo/procesar_zapatos.php
+++ modelo/procesar_zapatos.php
@@ -10,7 +10,7 @@ switch ($_REQUEST['req_zap'])
         $sucursal_idsucursal = $_POST['txt_idsucu'];
         
         $mensaje= Zapatos::insertarZapatos($marca, $modelo, $cantidad, $valor, $sucursal_idsucursal);
-        header('Location:./');
+        header('Location:../index.php');
         break;
     
     case 'Eliminar':
@@ -32,7 +32,7 @@ if(isset($_POST['txt_id']))
         $cantidad = $_POST['txt_cantidad'];
         $valor = $_POST['txt_valo'];
         //Colegio::editarColegio($id, $nombre, $estado, $ciudad);
-        header('Location:../gestiones/gestionarColegio.php');
+        header('Location:../index.php');
     }
     else
         echo 'id menor a 0';
