2b002fd5f7240a63153f6bcf904974f9bc6346a5
fernandatorres <fernandatorres1909@hotmail.com>
2015-11-24 12:08:26 -0500

    icionogeneral

---
+++ pastoIndex.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link type="text/css" rel="stylesheet" href="css/css.css">
+        <link type="text/css" rel="stylesheet" href="css/cssGen.css">
+        <title></title>
+    </head>
+    <body>
+        <?php include ("./header.php"); ?>
+        
+        <div>
+            
+             <div id="menu">
+            <nav>
+                <ul>
+                    <li ><a href="index.php"><img src="img/casa.png" width="25" height="25"></a></li>
+                    <li> <a href="vista_zapatosGN.php">Zapatos</a></li>
+                    <li> <a href="#">Vendendor</a></li>
+                    <li> <a href="#">Venta</a></li>                    
+                </ul>
+
+            </nav>
+        </div> 
+        
+        
+            <div id="content" class="center_content">
+            <img  src="img/images (1).jpg" onmouseover="this.src='img/images.jpg';" onmouseout="this.src='img/images (1).jpg';" width="1090px" height="990px"/>
+        </div>
+        
+       
+            </div>
+    </body>
+</html>
