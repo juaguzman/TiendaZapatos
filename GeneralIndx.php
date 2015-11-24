dbd2cf80329ea5341acb49cca2433bf05934db40
juaguzman <juancarlos_891111@hotmail.com>
2015-11-24 11:27:08 -0500

    indexGNr

---
+++ GeneralIndx.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <div id="menu">
+            <nav>
+                <ul>
+                    <li ><a href="index.php"><img src="img/casa.png" width="25" height="25"></a></li>
+                    <li> <a href="#">Zapatos</a></li>
+                    <li> <a href="#">Vendendor</a></li>
+                    <li> <a href="#">Venta</a></li>                    
+                </ul>
+
+            </nav>
+        </div>
+    </body>
+</html>
