ecfec31c404dccfd54d90daba1d2844b0e9fc0a4
juaguzman <juancarlos_891111@hotmail.com>
2015-11-24 01:00:15 -0500

    prsesarr sin metodos listos

--- procesar_zapatos.php
+++ procesar_zapatos.php
@@ -6,11 +6,37 @@ switch ($_REQUEST['req_zap'])
         $marca = $_POST['txt_marca'];
         $modelo = $_POST['txt_modelo'];
         $cantidad = $_POST['txt_cant'];
-        $valo = $_POST['txt_valor'];
+        $valor = $_POST['txt_valor'];
         $sucursal_idsucursal = $_POST['txt_idsucu'];
         
-        $mensaje= Zapatos::insertarZapatos($marca, $modelo, $cantidad, $valo, $sucursal_idsucursal);
-        
+        $mensaje= Zapatos::insertarZapatos($marca, $modelo, $cantidad, $valor, $sucursal_idsucursal);
+        header('Location:./');
         break;
+    
+    case 'Eliminar':
+        $id = $_REQUEST['id'];
+        
+        header('Location:');
+        break;
+    
+    case "Modificar":
+if(isset($_POST['txt_id']))
+{
+    $id= (int)$_POST['txt_id'];
+    
+    if($id>0)
+    {
+        
+         $marca = $_POST['txt_marca'];
+        $modelo = $_POST['txt_modelo'];
+        $cantidad = $_POST['txt_cantidad'];
+        $valor = $_POST['txt_valo'];
+        //Colegio::editarColegio($id, $nombre, $estado, $ciudad);
+        header('Location:../gestiones/gestionarColegio.php');
+    }
+    else
+        echo 'id menor a 0';
+}
+        
 }
 
