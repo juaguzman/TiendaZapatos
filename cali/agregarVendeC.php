ec7bfdb7f168e709658f2ee2ed869dbbaa987ee6
fernandatorres <fernandatorres1909@hotmail.com>
2015-11-25 02:25:45 -0500

    .....

--- cali/agregarVendeC.php
+++ cali/agregarVendeC.php
@@ -8,7 +8,7 @@
         <title></title>
     </head>
     <body>
-        <?php include ("../bogota/headerB.php"); ?>
+        <?php include ("../cali/headerC.php"); ?>
         
         <div>
             
@@ -16,19 +16,18 @@
             <nav>
                 <ul>
                     <li ><a href="index.php"><img src="../img/casa.png" width="25" height="25"></a></li>
-                    <li> <a href="vistaBogota.php">Zapatos</a></li>
-                    <li> <a href="vendedorB.php">Vendendor</a></li>
-                    <li> <a href="ventasB.php">Venta</a></li>                    
+                    <li> <a href="vistaCali.php">Zapatos</a></li>
+                    <li> <a href="vendedorCali.php">Vendendor</a></li>
+                    <li> <a href="ventasC.php">Venta</a></li>                    
                 </ul>
 
             </nav>
         </div> 
-        
-        
             <div id="content" class="center_content">
                 <div class="datagrid">
                     <form action="../modelo/procesar_vendedor.php" method="post">
-            <table border="1">
+                        
+                        <table border="1">
                 <thead>
                     <tr align ="center">
                         <th colspan="2">Agregar Vendedor</th> <!-- titulos de el formulario -->
@@ -48,10 +47,10 @@
                         <td><input type="text" name="txt_apellido" value="" required=""/></td>
                     </tr>
                     <tr>
-                        <td>Fecha nacimiento:</td>
+                        <td>Fecha:</td>
                         <td><input type="date" name="txt_fecha" value="" required=""/></td>
                     </tr>
-                     <input hidden="" type="hidden" name="txt_idsucu" value="3" />
+                     <input hidden="" type="hidden" name="txt_idsucu" value="1" />
                     
                     
                 </tbody>
@@ -60,18 +59,24 @@
             <table>
                 <tr>
                     <td>
-            <input type="submit" value="Enviar" class="boton"/>
-            <input type="hidden" value="Enviar" name="req_vende" >
-            </td>
-            </tr>
+                        <input type="submit" value="Enviar" class="boton"/>
+                        <input type="hidden" value="Enviar" name="req_vende" >
+                    </td>
+                </tr>
             </table>
 
           
-                    </div>
+            </div>   
         </div>
+           
+        
+        
+            
         
        
             </div>
     </body>
 </html>
+
+
 
