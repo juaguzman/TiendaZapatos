ee9a5ace25952307d68f69ad87ff6348ef0620b1
juaguzman <juancarlos_891111@hotmail.com>
2015-11-24 22:39:36 -0500

    procesasr ventas

--- modelo/ventas.php
+++ modelo/ventas.php
@@ -49,12 +49,50 @@ static function listar_ventas()
       while ($campo=mysqli_fetch_object($result)) 
                 {
              echo "<tr id=resul><td>$campo->idfactura</td><td>$campo->fecha</td><td>$campo->nomcomprador</td><td>$campo->cedulaComprador</td><td>$campo->vendedor</td><td>$campo->vendedor_cedula</td><td>$campo->idzapatos</td>"
-                     . "<td>$campo->marca</td><td>$campo->modelo</td><td>$campo->valoUnid</td><td>$campo->valor_total</td><td><a href=../modelo/procesar_zapatos.php?req_zap=Eliminar&id=".$campo->idzapatos.";>Elminar</a><a>Modificar</a> </tr> \n";
+                     . "<td>$campo->marca</td><td>$campo->modelo</td><td>$campo->valoUnid</td><td>$campo->valor_total</td><td>$campo->sucursal</td><td><a href=../modelo/procesar_zapatos.php?req_zap=Eliminar&id=".$campo->idfactura.";>Elminar</a><a>Modificar</a> </tr> \n";
                 }
                  echo "</table> \n";
                  $mysqli->close();
      
 }
+
+static function listar_ventas_sucursal($sucursal)
+{
+    include '../conexi.php';
+    $mysql = new conexion();
+    $mysqli=$mysql->conctar();
+    $consulta ="SELECT idfactura, fecha, valor_total, nomcomprador, cedulacomprador, vendedor_cedula, zapatos_idzapatos as 'idzapatos', zapatos.marca as 'marca', zapatos.modelo as 'modelo', zapatos.valor as 'calorUnid', vendedor.nombres as 'vendedor'"
+            . ", sucursal.nombre as 'sucursal' FROM zapatos, venta, vendedor, sucursal WHERE zapatos.idzapatos = venta.zapatos_idzapatos AND vendedor.cedula = venta.vendedor_cedula AND sucursal.idsucursal = vendedor.sucursal_idsucursal AND idsucursal = $sucursal";
+    $result = $mysqli->query($consulta);
+     echo "<table border = '3' id=res > \n";
+     echo "<tr align =center> <th colspan=7>Lista de Ventas Global</th> </tr>";
+     echo "<tr align=center id=tit><td >&nbsp;Factura #&nbsp;</td><td>&nbsp;Fecha&nbsp;</td><td>&nbsp;Comprador&nbsp;</td><td>&nbsp;Cedula&nbsp;</td><td>&nbsp;Vendedor&nbsp;</td><td>&nbsp;Cedula vend&nbsp;</td><td>&nbsp;ID Producto&nbsp;</td>"
+     . "<td>&nbsp;Marca&nbsp;</td><td>&nbsp;Modelo&nbsp;</td><td>&nbsp;Valor Unid&nbsp;</td><td>&nbsp;Valor Total&nbsp;</td><td>&nbsp;OPCIONES&nbsp;</td></tr> \n";
+      while ($campo=mysqli_fetch_object($result)) 
+                {
+             echo "<tr id=resul><td>$campo->idfactura</td><td>$campo->fecha</td><td>$campo->nomcomprador</td><td>$campo->cedulaComprador</td><td>$campo->vendedor</td><td>$campo->vendedor_cedula</td><td>$campo->idzapatos</td>"
+                     . "<td>$campo->marca</td><td>$campo->modelo</td><td>$campo->valoUnid</td><td>$campo->valor_total</td><td><a href=../modelo/procesar_zapatos.php?req_zap=Eliminar&id=".$campo->idfactura.";>Elminar</a><a>Modificar</a> </tr> \n";
+                }
+                 echo "</table> \n";
+                 $mysqli->close();
+     
+}
+
+static function eliminar_factura($id)
+{
+    include_once '../conex.php';
+    $mensaje="Resultado";
+    $sql = @mysql_query("Delete FROM venta WHERE idfactura = $id");
+     if (!$sql) {
+            $mensaje.="Error Eliminando factura en la base de datos: " . mysql_error();
+        } else {
+            $mensaje.="La factura con identificacion " . $id . " fueron eliminados del sistema";
+            
+        }
+        
+        return $mensaje;
+}
+
 
 
 
